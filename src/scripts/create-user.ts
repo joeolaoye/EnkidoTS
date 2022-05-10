f08a3939e4dd5e2b637ddbd7d88024abebc924f0
Joseph Olaoye <olaoye.joseph@gmail.com>
2022-05-10 23:06:07 +0100

    updated create-user script

--- src/scripts/create-user.ts
+++ src/scripts/create-user.ts
@@ -10,8 +10,11 @@ export const schema = {
   properties: {
     email: { type: 'string', format: 'email' },
     password: { type: 'string' },
+    fullname: {type: 'string'},
+    address: {type: 'string'},
+    phonenumber: {type: 'string'}
   },
-  required: ['email', 'password'],
+  required: ['email', 'password', 'fullname'],
   type: 'object',
 };
 
@@ -22,6 +25,11 @@ export async function main(args) {
     const user = new User();
     user.email = args.email;
     user.password = await hashPassword(args.password);
+    user.activated = false;
+    user.datelastlogin = new Date().toLocaleDateString();
+    user.fullname = args.fullname;
+    user.address = args.address;
+    user.phonenumber= args.phonenumber;
 
     console.log(await user.save());
   } catch (error) {
