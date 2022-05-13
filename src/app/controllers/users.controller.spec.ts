63038fdbf15abda561e12749bbebcee06bf0086f
Joseph Olaoye <olaoye.joseph@gmail.com>
2022-05-13 21:35:41 +0100

    update user controller test

--- src/app/controllers/users.controller.spec.ts
+++ src/app/controllers/users.controller.spec.ts
@@ -22,7 +22,7 @@ describe('UsersController', () => {
 
     it('should return an HttpResponseOK.', () => {
       const ctx = new Context({});
-      ok(isHttpResponseOK(controller.foo(ctx)));
+      ok(isHttpResponseOK(controller.getAllUsers(ctx)));
     });
 
   });
